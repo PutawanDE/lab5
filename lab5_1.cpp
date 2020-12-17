807a37be255db8ad4158b91140002ab662ee96a9
Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2020-12-17 17:41:51 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -1,9 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int input = 1;
+    int evenCount = 0;
+    int oddCount = 0;
+
+    while (input != 0){
+        cout << "Enter an integer: ";
+        cin >> input;
+        
+        if(input == 0){
+            break;
+        }
+        
+        if(input % 2 == 0){
+            evenCount++;
+        } else {
+            oddCount++;
+        }
+    }
+
+    cout << "#Even numbers = " << evenCount << "\n";
+    cout << "#Odd numbers = " << oddCount << "\n";
     return 0;
 }
